d992570da5ad0ac3cd0b8174cad811c9c3163058
abdullaziz278 <83576729+abdullaziz278@users.noreply.github.com>
2022-02-23 13:23:17 +0500

    hassan-admind-logoChange

--- client/src/layouts/dashboard/DashboardSidebar.js
+++ client/src/layouts/dashboard/DashboardSidebar.js
@@ -1,66 +1,66 @@
-import PropTypes from 'prop-types';
-import { useEffect } from 'react';
-import { Link as RouterLink, useLocation } from 'react-router-dom';
+import PropTypes from "prop-types"
+import { useEffect } from "react"
+import { Link as RouterLink, useLocation } from "react-router-dom"
 // material
-import { styled } from '@mui/material/styles';
-import { Box, Link, Drawer, Typography, Avatar } from '@mui/material';
+import { styled } from "@mui/material/styles"
+import { Box, Link, Drawer, Typography, Avatar } from "@mui/material"
 // components
-import Logo from '../../components/Logo';
-import Scrollbar from '../../components/Scrollbar';
-import NavSection from '../../components/NavSection';
-import { MHidden } from '../../components/@material-extend';
+import Logo from "../../components/Logo"
+import Scrollbar from "../../components/Scrollbar"
+import NavSection from "../../components/NavSection"
+import { MHidden } from "../../components/@material-extend"
 //
-import sidebarConfig from './SidebarConfig';
-import account from '../../_mocks_/account';
+import sidebarConfig from "./SidebarConfig"
+import account from "../../_mocks_/account"
 
 // ----------------------------------------------------------------------
 
-const DRAWER_WIDTH = 280;
+const DRAWER_WIDTH = 280
 
-const RootStyle = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('lg')]: {
+const RootStyle = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("lg")]: {
     flexShrink: 0,
-    width: DRAWER_WIDTH
-  }
-}));
+    width: DRAWER_WIDTH,
+  },
+}))
 
-const AccountStyle = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const AccountStyle = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(2, 2.5),
   borderRadius: theme.shape.borderRadiusSm,
-  backgroundColor: theme.palette.grey[200]
-}));
+  backgroundColor: theme.palette.grey[200],
+}))
 
 // ----------------------------------------------------------------------
 
 DashboardSidebar.propTypes = {
   isOpenSidebar: PropTypes.bool,
-  onCloseSidebar: PropTypes.func
-};
+  onCloseSidebar: PropTypes.func,
+}
 
 export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
-  const { pathname } = useLocation();
+  const { pathname } = useLocation()
   const adminName = sessionStorage.getItem("name")
   const adminRole = sessionStorage.getItem("isRole")
 
   useEffect(() => {
     if (isOpenSidebar) {
-      onCloseSidebar();
+      onCloseSidebar()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname]);
+  }, [pathname])
 
   const renderContent = (
     <Scrollbar
       sx={{
-        height: '100%',
-        '& .simplebar-content': { height: '100%', display: 'flex', flexDirection: 'column' }
+        height: "100%",
+        "& .simplebar-content": { height: "100%", display: "flex", flexDirection: "column" },
       }}
     >
       <Box sx={{ px: 2.5, py: 3 }}>
-        <Box component={RouterLink} to="/" sx={{ display: 'inline-flex' }}>
-          <Logo />
+        <Box component={RouterLink} to="/" sx={{ display: "inline-flex" }}>
+            <Logo />
         </Box>
       </Box>
 
@@ -69,10 +69,10 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
           <AccountStyle>
             <Avatar src={account.photoURL} alt="photoURL" />
             <Box sx={{ ml: 2 }}>
-              <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
+              <Typography variant="subtitle2" sx={{ color: "text.primary" }}>
                 {adminName}
               </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
                 {adminRole}
               </Typography>
             </Box>
@@ -122,7 +122,7 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
         </Stack>
       </Box> */}
     </Scrollbar>
-  );
+  )
 
   return (
     <RootStyle>
@@ -131,7 +131,7 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
           open={isOpenSidebar}
           onClose={onCloseSidebar}
           PaperProps={{
-            sx: { width: DRAWER_WIDTH }
+            sx: { width: DRAWER_WIDTH },
           }}
         >
           {renderContent}
@@ -145,13 +145,13 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
           PaperProps={{
             sx: {
               width: DRAWER_WIDTH,
-              bgcolor: 'background.default'
-            }
+              bgcolor: "background.default",
+            },
           }}
         >
           {renderContent}
         </Drawer>
       </MHidden>
     </RootStyle>
-  );
+  )
 }
