ef111f28f9bf06dafba8bcdb6bc88374e30dd02d
PakDragoon <pagan1581998@gmail.com>
2022-03-02 17:43:49 +0500

    update-02-03-2022

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://127.0.0.1:27017/async-now', {})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/async-now", {})
+  .then(() => {
+    console.log("Connected to database ")
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`)
+  })
 
-// const password = "qwert12345"
+// const password = `tdfxd0aog3QYV94x`
 
-// const url = `mongodb+srv://user1:${password}@cluster0.ld4pg.mongodb.net/async-now?retryWrites=true&w=majority`
-
-// const connectionParams={
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// }
+// const url = `mongodb+srv://user1:${password}@cluster0.ld4pg.mongodb.net/async-now?retryWrites=true&w=majority&ssl=true`
 
 // mongoose
 //   .connect(url, {})
